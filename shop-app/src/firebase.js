53c904e7e8355dd9769bbb6120d144f0dff1bf72
soyeon <jayy_19@ewhain.net>
2024-09-29 20:41:54 +0900

    Feat: Add authentication system with firebase

--- shop-app/src/firebase.js
+++ shop-app/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCxUW5zu4HyJXanLoRY8uqOTsDZf6UxodQ',
-  authDomain: 'goorm-shop-app.firebaseapp.com',
-  projectId: 'goorm-shop-app',
-  storageBucket: 'goorm-shop-app.appspot.com',
-  messagingSenderId: '984805235752',
-  appId: '1:984805235752:web:45ebd22fc8b2f940b8291d',
-  measurementId: 'G-1KC2CBDLM7',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
